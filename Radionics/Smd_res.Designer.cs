1763a4cfdadb22d36dd5c09b98ee5288b591934c
Vadym <shaller555@gmail.com>
2022-06-24 01:49:06 +0300

    change9

--- Radionics/Smd_res.Designer.cs
+++ Radionics/Smd_res.Designer.cs
@@ -37,8 +37,8 @@ namespace Radionics
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.Count = new System.Windows.Forms.Button();
+            this.submit = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -46,7 +46,7 @@ namespace Radionics
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Radionics.Properties.Resources.smd_res;
-            this.pictureBox1.Location = new System.Drawing.Point(49, 46);
+            this.pictureBox1.Location = new System.Drawing.Point(85, 44);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(276, 155);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
@@ -58,99 +58,102 @@ namespace Radionics
             this.textBox1.BackColor = System.Drawing.Color.DimGray;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.ForeColor = System.Drawing.Color.White;
-            this.textBox1.Location = new System.Drawing.Point(149, 118);
+            this.textBox1.Location = new System.Drawing.Point(187, 117);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(63, 15);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(46, 230);
+            this.label1.Location = new System.Drawing.Point(27, 230);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(80, 17);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "Результат:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(126, 230);
+            this.label2.Location = new System.Drawing.Point(113, 230);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.Size = new System.Drawing.Size(0, 17);
             this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(156, 9);
+            this.label3.Location = new System.Drawing.Point(184, 9);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.Size = new System.Drawing.Size(64, 17);
             this.label3.TabIndex = 4;
-            this.label3.Text = "label3";
+            this.label3.Text = "код → Ω";
             // 
             // label4
             // 
             this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label4.Location = new System.Drawing.Point(27, 267);
+            this.label4.Location = new System.Drawing.Point(27, 273);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(338, 2);
+            this.label4.Size = new System.Drawing.Size(375, 2);
             this.label4.TabIndex = 5;
             this.label4.Text = "label4";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(33, 327);
+            this.label5.Location = new System.Drawing.Point(0, 321);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.Size = new System.Drawing.Size(133, 17);
             this.label5.TabIndex = 6;
-            this.label5.Text = "label5";
+            this.label5.Text = "Введіть опір(в Ом):";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(108, 322);
+            this.textBox2.Location = new System.Drawing.Point(139, 321);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(167, 22);
             this.textBox2.TabIndex = 7;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
             // 
-            // button1
+            // Count
             // 
-            this.button1.Location = new System.Drawing.Point(250, 224);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Count.Location = new System.Drawing.Point(256, 224);
+            this.Count.Name = "Count";
+            this.Count.Size = new System.Drawing.Size(105, 29);
+            this.Count.TabIndex = 8;
+            this.Count.Text = "Підрахувати";
+            this.Count.UseVisualStyleBackColor = true;
+            this.Count.Click += new System.EventHandler(this.Count_Click);
             // 
-            // button2
+            // submit
             // 
-            this.button2.Location = new System.Drawing.Point(283, 321);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.submit.Location = new System.Drawing.Point(312, 318);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(105, 25);
+            this.submit.TabIndex = 9;
+            this.submit.Text = "Підтвердити";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(156, 286);
+            this.label6.Location = new System.Drawing.Point(184, 288);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.Size = new System.Drawing.Size(64, 17);
             this.label6.TabIndex = 10;
-            this.label6.Text = "label6";
+            this.label6.Text = "Ω → код";
             // 
             // Smd_res
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(379, 375);
+            this.ClientSize = new System.Drawing.Size(429, 375);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.Count);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -161,6 +164,7 @@ namespace Radionics
             this.Controls.Add(this.pictureBox1);
             this.Name = "Smd_res";
             this.Text = "Smd_res";
+            this.Load += new System.EventHandler(this.Smd_res_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -177,8 +181,8 @@ namespace Radionics
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Count;
+        private System.Windows.Forms.Button submit;
         private System.Windows.Forms.Label label6;
     }
 }
--- Radionics/Smd_res.cs
+++ Radionics/Smd_res.cs
@@ -16,5 +16,64 @@ namespace Radionics
         {
             InitializeComponent();
         }
+
+        private void Smd_res_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string text = textBox1.Text;
+            char ch = e.KeyChar;
+            if (ch != 8)
+            {
+                if (!Char.IsDigit(ch) || text.Length > 2)
+                {
+                    e.Handled = true;
+                }
+            }
+            
+        }
+
+        private void Count_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            if (text.Length == 3)
+            {
+                string value1;
+                string value2 = text[2].ToString();
+                if (text.StartsWith("0"))
+                    value1 = text[1].ToString();
+
+                else
+                    value1 = text.Substring(0, 2);
+                label2.Text = (Convert.ToInt32(value1) * Math.Pow(10, Convert.ToInt32(value2))).ToString() + " Oм";
+            }
+            else
+                MessageBox.Show("Потрібно 3 числа!");
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string text = textBox2.Text;
+            char ch = e.KeyChar;
+            if (ch!=8)
+            {
+                if (!Char.IsDigit(ch) || text.Length > 10 )
+                {
+                    e.Handled = true;
+                }
+                if (text.CompareTo("")==0 && ch=='0')
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox2.Text.Substring(0, 2) + (textBox2.Text.Length - 2).ToString();
+        }
     }
 }
